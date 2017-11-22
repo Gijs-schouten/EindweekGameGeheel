9914e744602a6892826ef3a24b4f6d9441a280e3
Gijs Schouten <gijswow@gmail.com>
2017-11-22 16:06:28 +0100

    alles

---
+++ Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public Button Yes;
+	public Button No;
+
+	public bool _isDead;
+
+	void Start(){
+		Yes.onClick.AddListener(delegate() { TaskOnClick("Play"); });
+		No.onClick.AddListener(delegate() { TaskOnClick("Back"); });
+	}
+
+	void TaskOnClick(string option){
+		if (option == "Play") {
+			SceneManager.LoadScene ("Game");
+			Debug.Log ("Play");
+		} else if (option == "Back") {
+			SceneManager.LoadScene ("Menu");
+		}
+	}
+}
